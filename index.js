b062b31aad11bc362cf27c45ce69a1586484c9c0
bredele <olivier.wietrich@gmail.com>
2018-12-18 12:56:52 -0700

    Add documentation

--- index.js
+++ index.js
@@ -5,6 +5,15 @@
 const url = require('url')
 const querystring = require('querystring')
 
+/**
+ * Strictly match URI query parameters with given object.
+ *
+ * @param {String} uri
+ * @param {Object} object
+ * @return {Boolean} true if strictly match
+ * @api public
+ */
+
 module.exports = (uri, object) => {
   let result = false
   if (object && typeof object === 'object') {
@@ -14,6 +23,15 @@ module.exports = (uri, object) => {
   return result
 }
 
+/**
+ * Return true if second object contains all keys and values
+ * from the first object.
+ *
+ * @param {Object} obj1
+ * @param {Object} obj2
+ * @return {Boolean}
+ * @api private
+ */
 
 function contains (obj1, obj2) {
   const keys = Object.keys(obj1)
@@ -22,10 +40,6 @@ function contains (obj1, obj2) {
   while (l--) {
     const key = keys[l]
     result = result && obj1[key] === obj2[key]
-    // if (obj1[key] === obj2[key]) {
-    //   result = true
-    //   break
-    // }
   }
   return result
 }
