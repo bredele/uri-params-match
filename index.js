688b7a660a4ce2a29fd8b0dfddcef55af23450a6
bredele <olivier.wietrich@gmail.com>
2018-12-18 12:47:33 -0700

    Match query parameters with given object

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+/**
+ * Dependencies
+ */
 
-module.exports = () => {
-  
+const url = require('url')
+const querystring = require('querystring')
+
+module.exports = (uri, object) => {
+  let result = false
+  if (object && typeof object === 'object') {
+    const parser = url.parse(uri)
+    result = contains(querystring.parse(parser.query), object)
+  }
+  return result
+}
+
+
+function contains (obj1, obj2) {
+  let result = false
+  const keys = Object.keys(obj1)
+  let l = keys.length
+  while (l--) {
+    const key = keys[l]
+    if (obj1[key] === obj2[key]) {
+      result = true
+      break
+    }
+  }
+  return result
 }
